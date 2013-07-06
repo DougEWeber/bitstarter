d060195e0c226a9ee2a528ae1a71af756e975fbc
Douglas Weber <d.e.weber@att.net>
2013-07-06 17:51:04 +0000

    first pass web.js wpassenter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var titleText = fs.readFile('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(titleText);
 });
 
 var port = process.env.PORT || 5000;
