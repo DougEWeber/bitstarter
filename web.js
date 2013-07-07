d2e3352a060b5aa3b3d21bf17a1e321eb6ebd8b9
Douglas Weber <d.e.weber@att.net>
2013-07-07 18:24:05 +0000

    home got it right

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var titleBuffer = fs.readFile('index.html');
+var titleBuffer = fs.readFileSync('./index.html');
 var titleText = titleBuffer.toString();
 app.get('/', function(request, response) {
   response.send(titleText);
 });
 
-console.log(titleText);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
