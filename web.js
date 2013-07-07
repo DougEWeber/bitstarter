1ded444a3a2fe0c98ec3a4c9439f91b587a97283
Douglas Weber <d.e.weber@att.net>
2013-07-07 18:04:49 +0000

    console log titletest

--- web.js
+++ web.js
@@ -7,6 +7,8 @@ app.get('/', function(request, response) {
   response.send('Hi World');
 });
 
+console.log(titleText);
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
